6ff5271884f3ea20f6caeeca39c3bdc595f38769
Joe Stewart <joe.stewart@mediacurrent.com>
2019-07-18 17:35:43 -0400

    MCT-507 Add VRT testing of styleguide

--- tests/visual-regression/paths.js
+++ tests/visual-regression/paths.js
@@ -2,6 +2,11 @@ var pathConfig = {};
 
 pathConfig.array = [
   '/', // homepage
+  '/themes/contrib/rain_theme/dist/style-guide/', //styleguide overview
+  '/themes/contrib/rain_theme/dist/style-guide/section-components.html', //styleguide components
+  '/themes/contrib/rain_theme/dist/style-guide/section-icons.html', //styleguide icons
+  '/themes/contrib/rain_theme/dist/style-guide/section-colors.html', //styleguide colors
+  '/themes/contrib/rain_theme/dist/style-guide/section-type.html', //styleguide colors
   // include other relative URL paths in this array
 ]
 
