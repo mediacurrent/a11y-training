f78e4477259b1674be29197e2933b6edeb0821dd
Joe Stewart <joe.stewart@mediacurrent.com>
2019-07-23 16:14:00 -0400

    MCT-507 Limit styleguide path to the components page

--- tests/visual-regression/paths.js
+++ tests/visual-regression/paths.js
@@ -2,11 +2,7 @@ var pathConfig = {};
 
 pathConfig.array = [
   '/', // homepage
-  '/themes/contrib/rain_theme/dist/style-guide/', //styleguide overview
   '/themes/contrib/rain_theme/dist/style-guide/section-components.html', //styleguide components
-  '/themes/contrib/rain_theme/dist/style-guide/section-icons.html', //styleguide icons
-  '/themes/contrib/rain_theme/dist/style-guide/section-colors.html', //styleguide colors
-  '/themes/contrib/rain_theme/dist/style-guide/section-type.html', //styleguide type
   // include other relative URL paths in this array
 ]
 
