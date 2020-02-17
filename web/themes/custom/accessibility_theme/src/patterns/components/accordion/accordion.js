4e1cdc12d38bd904c7cdb7669b8eed1f52f97956
Mario Hernandez <mario.hernandez@mediacurrent.com>
2020-02-16 16:13:47 -0800

    completes theming and integration of accordion

--- web/themes/custom/accessibility_theme/src/patterns/components/accordion/accordion.js
+++ web/themes/custom/accessibility_theme/src/patterns/components/accordion/accordion.js
@@ -1,40 +1,44 @@
-/* eslint-disable max-len */
 !((document, Drupal, $) => {
   'use strict';
 
+  /**
+   * Creates behavior for footer menus to allow for open/close functionality.
+   */
   Drupal.behaviors.accordion = {
     attach: function(context) {
-      const self = this;
-      const $trigger = $('.accordion__toggle', context);
+      /**
+       * Bind open/close action to list containers.
+       * @param {string} container - Accordion conteainer.
+       * @param {string} button - Toggle button.
+       * @param {context} context - Drupal context.
+       * @returns {void}
+       */
+      function accordionItems(container, button, context) {
+        const $container = $(container, context);
+        const $buttons = $container.find(button);
 
-      if (!$trigger.length) {
-        return;
+        $container.each(function() {
+          const $accordionItem = $(this);
+          $accordionItem.find(button).on('click', function() {
+            const $self = $(this);
+            // Removes the 'is-open' class from all accordion items
+            // except the one we are currently clicking on.
+            $container.not($accordionItem).removeClass('is-open');
+            // Toggles on/off the 'is-open' tag on items we click on.
+            $accordionItem.toggleClass('is-open');
+            // Sets aria attributes to false on any items other
+            // the one one we are clicking on.
+            $buttons.not($self).attr('aria-expanded', 'false');
+            $buttons.not($self).attr('aria-selected', 'false');
+            // Sets aria attributes to tru on the item we click on.
+            $self.attr('aria-expanded', 'true');
+            $self.attr('aria-selected', 'true');
+          });
+        });
       }
 
-      $trigger.once('accordion-trigger').on('click', function() {
-        self.toggleState($(this));
-      });
-    },
-
-    toggleState: function($toggle) {
-      const $accordionContainer = $toggle.closest('.accordion__items');
-      const toggleExpandedValue = $toggle.attr('aria-expanded');
-
-      $toggle.attr(
-        'aria-expanded',
-        toggleExpandedValue === 'true' ? 'false' : 'true'
-      );
-
-      const $accordionContent = $(
-        '#' + $toggle.attr('aria-controls'),
-        $accordionContainer
-      );
-      const accordionHiddenValue = $accordionContent.attr('aria-hidden');
-
-      $accordionContent.attr(
-        'aria-hidden',
-        accordionHiddenValue === 'true' ? 'false' : 'true'
-      );
+      // Bind the function to the accordion.
+      accordionItems('.accordion__item', '.accordion__toggle', context);
     }
   };
 })(document, Drupal, jQuery);
